de556bd17ff54343e647ac29c9d9c69b64afce2a
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-28 14:00:22 +0200

    [ADD] Implement AmbientLight class with constructor and CMake configuration

---
+++ src/lights/AmbientLight/AmbientLight.hpp
@@ -0,0 +1,19 @@
+//
+// Created by roussierenoa on 4/28/25.
+//
+
+#ifndef AMBIENTLIGHT_HPP
+#define AMBIENTLIGHT_HPP
+
+
+namespace Raytracer::light {
+    class AmbientLight {
+    public:
+        AmbientLight();
+        ~AmbientLight() = default;
+    };
+}
+
+
+
+#endif //AMBIENTLIGHT_HPP
