bc7edc4dedd990053a0a1ba290ca30d55235f31e
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-28 14:09:11 +0200

    [ADD] Implement Sphere class with constructor and CMake configuration

---
+++ src/primitives/Sphere/Sphere.hpp
@@ -0,0 +1,17 @@
+//
+// Created by roussierenoa on 4/28/25.
+//
+
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+namespace Raytracer::primitive {
+
+class Sphere {
+    Sphere();
+    ~Sphere() = default;
+};
+
+} // Raytracer
+
+#endif //SPHERE_HPP
