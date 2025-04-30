fcaa9175753c729f62136bfb2fbbf107341f7daa
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-30 10:44:46 +0200

    [ADD] Adding default parsing file, and all the compilation

---
+++ src/Parser/Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roussierenoa on 4/30/25.
+//
+
+#include "Parser.hpp"
---
+++ src/Parser/Parser.hpp
@@ -0,0 +1,20 @@
+//
+// Created by roussierenoa on 4/30/25.
+//
+
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+
+
+namespace raytracer {
+    class Parser {
+    public:
+        Parser();
+        ~Parser() = default;
+    };
+}
+
+
+
+#endif //PARSER_HPP
