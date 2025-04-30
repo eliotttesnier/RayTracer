be23fb0944ceb1b82a269835983fa8702c1bd671
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-04-30 14:55:06 +0200

    [ADD] Implement camera, light, and primitive configuration parsing in Parser class

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -3,3 +3,105 @@
 //
 
 #include "Parser.hpp"
+#include <iostream>
+#include <string>
+#include <libconfig.h++>
+
+void raytracer::Parser::_getCameraData(const libconfig::Setting &root) {
+    const auto &camera = root["camera"];
+    const auto &res = camera["resolution"];
+    const auto &pos = camera["position"];
+    const auto &rot = camera["rotation"];
+
+    int width = res.lookup("width");
+    int height = res.lookup("height");
+    double x = pos.lookup("x");
+    double y = pos.lookup("y");
+    double z = pos.lookup("z");
+
+    double rx = rot.lookup("x");
+    double ry = rot.lookup("y");
+    double rz = rot.lookup("z");
+    double fov = camera["fieldOfView"];
+#ifdef _DEBUG
+    std::cout << "Camera: " << width << "x" << height << ", pos(" << x << ", " << y << ", " << z << "), rota(" << rx << ", " << ry << ", " << rz << "), fov(" << fov << ")" << std::endl;;
+#endif
+    this->_camConfig = std::make_unique<CameraConfig>(std::make_tuple(width, height), std::make_tuple(x, y, z), std::make_tuple(rx, ry, rz), fov);
+}
+
+void raytracer::Parser::_getPrimitivesData(const libconfig::Setting &root) {
+    std::vector<std::pair<std::tuple<double, double, double, double>, std::tuple<int, int, int>>> spheresVector;
+    const auto &spheres = root["primitives"]["spheres"];
+    for (int i = 0; i < spheres.getLength(); ++i) {
+        const auto &s = spheres[i];
+        double px = s["x"];
+        double py = s["y"];
+        double pz = s["z"];
+        double radius = s["r"];
+        const auto &color = s["color"];
+        int cr = color["r"], cg = color["g"], cb = color["b"];
+        spheresVector.emplace_back(std::make_tuple(px, py, pz, radius), std::make_tuple(cr, cg, cb));
+#ifdef _DEBUG
+        std::cout << "Sphere: pos(" << px << ", " << py << ", " << pz << "), r=" << radius
+                  << ", color(" << cr << ", " << cg << ", " << cb << ")\n";
+#endif
+    }
+    std::vector<std::tuple<char, double, std::tuple<int, int, int>>> planesVector;
+    const auto &planes = root["primitives"]["planes"];
+    for (int i = 0; i < planes.getLength(); ++i) {
+        const auto &p = planes[i];
+        std::string axis = p["axis"];
+        double pos = p["position"];
+        const auto &color = p["color"];
+        int cr = color["r"];
+        int cg = color["g"];
+        int cb = color["b"];
+        planesVector.emplace_back(axis[0], pos, std::make_tuple(cr, cg, cb));
+#ifdef _DEBUG
+        std::cout << "Plane: axis=" << axis << ", position=" << pos
+                  << ", color(" << cr << ", " << cg << ", " << cb << ")\n";
+#endif
+    }
+    this->_primitiveConfig = std::make_unique<PrimitiveConfig>(spheresVector, planesVector);
+}
+
+void raytracer::Parser::_getLightsData(const libconfig::Setting &root) {
+    const auto &lights = root["lights"];
+    double ambient = lights["ambient"];
+    double diffuse = lights["diffuse"];
+
+    const auto &point = lights["point"];
+    const auto &pt = point[0];
+    double px = pt.lookup("x");
+    double py = pt.lookup("y");
+    double pz = pt.lookup("z");
+#ifdef _DEBUG
+    std::cout << "Lights: ambient=" << ambient << ", diffuse=" << diffuse << ", pos(" << px << ", " << py << ", " << pz << ")" << std::endl;
+#endif
+    this->_lightConfig = std::make_unique<LightConfig>(std::vector<std::tuple<double, double, double>>(), std::make_tuple(px, py, pz), ambient, diffuse);
+}
+
+raytracer::Parser::Parser(char *path) {
+    libconfig::Config cfg;
+
+    try {
+        cfg.readFile(path);
+    } catch (const libconfig::FileIOException &e) {
+        std::cerr << "[ERROR] I/O error while reading config file: " << path << "\n";
+        throw;
+    } catch (const libconfig::ParseException &e) {
+        std::cerr << "[ERROR] Parse error at " << e.getFile() << ":" << e.getLine()
+                  << " - " << e.getError() << "\n";
+        throw;
+    }
+
+    try {
+        const libconfig::Setting &root = cfg.getRoot();
+        this->_getCameraData(root);
+        this->_getLightsData(root);
+        this->_getPrimitivesData(root);
+    } catch (const libconfig::SettingNotFoundException &e) {
+        std::cerr << "[ERROR] Missing setting: " << e.getPath() << "\n";
+        throw;
+    }
+}
--- src/Parser/Parser.hpp
+++ src/Parser/Parser.hpp
@@ -5,12 +5,66 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-
+#include <libconfig.h++>
+#include <memory>
+#include <vector>
 
 namespace raytracer {
-    class Parser {
+    class CameraConfig {
+    private:
+        const std::tuple<int, int> _resolution;
+        const std::tuple<double, double, double> _position;
+        const std::tuple<double, double, double> _rotation;
+        const double _fov;
     public:
-        Parser();
+        CameraConfig() = delete;
+        CameraConfig(const std::tuple<int, int> &resolution,
+                     const std::tuple<double, double, double> &position,
+                     const std::tuple<double, double, double> &rotation,
+                     const double fov) : _resolution(resolution), _position(position), _rotation(rotation), _fov(fov) {};
+        ~CameraConfig() = default;
+    };
+
+    class LightConfig {
+    private:
+        const std::vector<std::tuple<double, double, double>> _directional;
+        const std::tuple<double, double, double> _position;
+        const double _ambient;
+        const double _diffuse;
+    public:
+        LightConfig() = delete;
+        LightConfig(const std::vector<std::tuple<double, double, double>> &directional,
+                    const std::tuple<double, double, double> &position,
+                    const double ambient,
+                    const double diffuse) : _directional(directional), _position(position), _ambient(ambient), _diffuse(diffuse) {};
+        ~LightConfig() = default;
+    };
+
+
+    class PrimitiveConfig {
+        std::vector<std::pair<std::tuple<double, double, double, double>, std::tuple<int, int, int>>> _sphere; // Position + raduis, Color
+        std::vector<std::tuple<char, double, std::tuple<int, int, int>>> _plane; // axis, position, Color
+    public:
+        PrimitiveConfig() = delete;
+        PrimitiveConfig(const std::vector<std::pair<std::tuple<double, double, double, double>, std::tuple<int, int, int>>> &sphere,
+                        const std::vector<std::tuple<char, double, std::tuple<int, int, int>>> &plane) : _sphere(sphere), _plane(plane) {};
+        ~PrimitiveConfig() = default;
+    };
+
+    class Parser {
+    private:
+        std::unique_ptr<CameraConfig> _camConfig;
+        std::unique_ptr<LightConfig> _lightConfig;
+        std::unique_ptr<PrimitiveConfig> _primitiveConfig;
+
+        void _getCameraData(const libconfig::Setting &root);
+        void _getPrimitivesData(const libconfig::Setting &root);
+        void _getLightsData(const libconfig::Setting &root);
+    public:
+
+
+        Parser() = delete;
+        Parser(char *path);
         ~Parser() = default;
     };
 }
