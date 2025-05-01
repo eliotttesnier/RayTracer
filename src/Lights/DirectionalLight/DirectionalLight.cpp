9dd9930b38341daf2a90ea6f79f8d88c6b81fa93
albanrss <alban.roussee@epitech.eu>
2025-05-02 01:14:08 +0200

    [ADD] Lights: DirectionnalLight class created and implemented, without complete intersect function

--- src/Lights/DirectionalLight/DirectionalLight.cpp
+++ src/Lights/DirectionalLight/DirectionalLight.cpp
@@ -2,9 +2,34 @@
 // Created by roussierenoa on 4/28/25.
 //
 
-#include <iostream>
 #include "DirectionalLight.hpp"
+#include <iostream>
 
-Raytracer::light::DirectionalLight::DirectionalLight() {
-    std::cout << "DirectionalLight constructor called" << std::endl;
+using namespace Raytracer::light;
+
+DirectionalLight::DirectionalLight(const Math::Vector3D &direction)
+    : _direction(direction)
+{
+}
+
+void DirectionalLight::setDirection(const Math::Vector3D &direction)
+{
+    _direction = direction;
+}
+
+Math::Vector3D DirectionalLight::getDirection() const
+{
+    return _direction;
+}
+
+bool DirectionalLight::intersect(
+    const Math::Ray &ray,
+    Math::Point3D &hitPoint,
+    std::vector<std::shared_ptr<IPrimitive>> primitives
+) const
+{
+    (void)ray;
+    (void)hitPoint;
+    (void)primitives;
+    return true;
 }
