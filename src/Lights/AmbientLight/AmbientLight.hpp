452850592a9fb2fd95cdc5c2308ab9a9324655b4
albanrss <alban.roussee@epitech.eu>
2025-05-02 01:13:41 +0200

    [ADD] Lights: AmbientLight class created and implemented

--- src/Lights/AmbientLight/AmbientLight.hpp
+++ src/Lights/AmbientLight/AmbientLight.hpp
@@ -5,15 +5,23 @@
 #ifndef AMBIENTLIGHT_HPP
 #define AMBIENTLIGHT_HPP
 
+#include "../ILight.hpp"
+#include "../../Math/Ray.hpp"
+#include "../../Math/Point3D.hpp"
+#include "../../Lights/ALight.hpp"
 
 namespace Raytracer::light {
-    class AmbientLight {
-    public:
-        AmbientLight();
-        ~AmbientLight() = default;
+    class AmbientLight : public ALight {
+        public:
+            AmbientLight();
+            ~AmbientLight() = default;
+
+            bool intersect(
+                const Math::Ray &ray,
+                Math::Point3D &hitPoint,
+                std::vector<std::shared_ptr<IPrimitive>> primitives
+            ) const override;
     };
 }
-
-
 
 #endif //AMBIENTLIGHT_HPP
