2649f7e6156ab2097d9982167706a1e9185dd8d8
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-05-02 17:44:44 +0200

    [ADD] Implement LoaderType and entry points for AmbientLight and DirectionalLight; update CMakeLists for new modules

---
+++ src/LibLoader/LoaderType.hpp
@@ -0,0 +1,17 @@
+//
+// Created by roussierenoa on 5/2/25.
+//
+
+#ifndef LOADERTYPE_HPP
+#define LOADERTYPE_HPP
+
+namespace RayTracer::Loader
+{
+    enum ModuleType {
+        LIGHT_MODULE,
+        PRIMITIVE_MODULE,
+        TYPE_COUNT,
+    };
+}
+
+#endif //LOADERTYPE_HPP
---
+++ src/Lights/DirectionalLight/EntyPoint.cpp
@@ -0,0 +1,30 @@
+//
+// Created by roussierenoa on 5/2/25.
+//
+
+#include <iostream>
+#include <unistd.h>
+#include "LoaderType.hpp"
+#include "DirectionalLight.hpp"
+#include "../ILight.hpp"
+
+extern "C" {
+    ILight *create(void) {
+        return new RayTracer::light::DirectionalLight();
+    }
+    RayTracer::Loader::ModuleType getType(void) {
+        return RayTracer::Loader::ModuleType::LIGHT_MODULE;
+    }
+    __attribute__((constructor)) void construct()
+    {
+#ifdef _DEBUG
+        std::cout << "Loaded dynamic lib: Lib DirectionalLight" << std::endl;
+#endif
+    }
+    __attribute__((destructor)) void deconstruct()
+    {
+#ifdef _DEBUG
+        std::cout << "Unloaded dynamic lib: Lib DirectionalLight" << std::endl;
+#endif
+    }
+}
