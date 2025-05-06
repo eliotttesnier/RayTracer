f073acdbeb643430c638994c8cf94ad80261c037
albanrss <alban.roussee@epitech.eu>
2025-05-06 17:49:55 +0200

    [UPT] Primitives: updated to inherit from APrimitive

--- src/Primitives/IPrimitive.hpp
+++ src/Primitives/IPrimitive.hpp
@@ -9,9 +9,16 @@
 #define IPRIMITIVE_HPP_
 
 #include <string>
+#include <vector>
+#include <memory>
 
+#include "../Lights/ILight.hpp"
 #include "../Math/HitData.hpp"
 #include "../Math/Ray.hpp"
+#include "../Graphic/Color.hpp"
+
+class ILight;
+class IPrimitive;
 
 class IPrimitive {
     public:
@@ -25,6 +32,12 @@ class IPrimitive {
 
         // Methods
         virtual Math::hitdata_t intersect(const Math::Ray &ray) = 0;
+        virtual Graphic::color_t getColor(
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) = 0;
 };
 
 #endif /* !IPRIMITIVE_HPP_ */
