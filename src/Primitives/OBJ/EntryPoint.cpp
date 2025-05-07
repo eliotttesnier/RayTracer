904a0e315aa201198106f8ae5eae1ba9e8e6e4c7
Nolan Papa <nolan.papa@epitech.eu>
2025-05-07 15:11:27 +0200

    [FIX] Reorder includes and improve code formatting in EntryPoint.cpp for consistency

--- src/Primitives/OBJ/EntryPoint.cpp
+++ src/Primitives/OBJ/EntryPoint.cpp
@@ -5,28 +5,34 @@
 ** EntryPoint
 */
 
-#include <iostream>
 #include <unistd.h>
+
+#include <iostream>
+#include <string>
+
 #include "LoaderType.hpp"
 #include "OBJ.hpp"
 #include "../IPrimitive.hpp"
 
 extern "C" {
-    RayTracer::primitive::OBJ *create(const Math::Point3D &position, const std::string &filepath) {
+    RayTracer::primitive::OBJ *create(const Math::Point3D &position,
+                                      const std::string &filepath)
+    {
         return new RayTracer::primitive::OBJ(position, filepath);
     }
-    
-    RayTracer::Loader::ModuleType getType(void) {
+
+    RayTracer::Loader::ModuleType getType(void)
+    {
         return RayTracer::Loader::ModuleType::PRIMITIVE_MODULE;
     }
-    
+
     __attribute__((constructor)) void construct()
     {
 #ifdef _DEBUG
         std::cout << "Loaded dynamic lib: Lib OBJ" << std::endl;
 #endif
     }
-    
+
     __attribute__((destructor)) void deconstruct()
     {
 #ifdef _DEBUG
