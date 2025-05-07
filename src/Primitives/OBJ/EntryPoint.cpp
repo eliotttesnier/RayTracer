c1d0d5084571930a4693da5e5c627808baed1c0e
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-07 10:42:10 +0200

    [ADD] OBJ Primitive implementation

---
+++ src/Primitives/OBJ/EntryPoint.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2025
+** raytracer
+** File description:
+** EntryPoint
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include "LoaderType.hpp"
+#include "OBJ.hpp"
+#include "../IPrimitive.hpp"
+
+extern "C" {
+    RayTracer::primitive::OBJ *create(const Math::Point3D &position, const std::string &filepath) {
+        return new RayTracer::primitive::OBJ(position, filepath);
+    }
+    
+    RayTracer::Loader::ModuleType getType(void) {
+        return RayTracer::Loader::ModuleType::PRIMITIVE_MODULE;
+    }
+    
+    __attribute__((constructor)) void construct()
+    {
+#ifdef _DEBUG
+        std::cout << "Loaded dynamic lib: Lib OBJ" << std::endl;
+#endif
+    }
+    
+    __attribute__((destructor)) void deconstruct()
+    {
+#ifdef _DEBUG
+        std::cout << "Unloaded dynamic lib: Lib OBJ" << std::endl;
+#endif
+    }
+}
