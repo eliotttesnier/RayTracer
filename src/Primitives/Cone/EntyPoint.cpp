c92b9972b537c5b2005180c00f00a097bb826948
Nolan Papa <nolan.papa@epitech.eu>
2025-05-07 14:53:13 +0200

    [FIX] Reorder includes and improve code formatting in EntyPoint.cpp

--- src/Primitives/Cone/EntyPoint.cpp
+++ src/Primitives/Cone/EntyPoint.cpp
@@ -2,17 +2,23 @@
 // Created by roussierenoa on 5/2/25.
 //
 
-#include <iostream>
 #include <unistd.h>
+
+#include <iostream>
+
 #include "LoaderType.hpp"
 #include "Cone.hpp"
 #include "../IPrimitive.hpp"
 
 extern "C" {
-    RayTracer::primitive::Cone *create(const Math::Point3D &position, const double &radius, const double &height) {
+    RayTracer::primitive::Cone *create(const Math::Point3D &position,
+                                       const double &radius,
+                                       const double &height)
+    {
         return new RayTracer::primitive::Cone(position, radius, height);
     }
-    RayTracer::Loader::ModuleType getType(void) {
+    RayTracer::Loader::ModuleType getType(void)
+    {
         return RayTracer::Loader::ModuleType::PRIMITIVE_MODULE;
     }
     __attribute__((constructor)) void construct()
