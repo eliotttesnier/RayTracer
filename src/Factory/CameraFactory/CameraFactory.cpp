d80e34e6aa328d75021f6d3fb95722e85df99cfd
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-05-07 17:08:18 +0200

    [FIX] Optimize camera creation by calculating aspect ratio and updating screen parameters

--- src/Factory/CameraFactory/CameraFactory.cpp
+++ src/Factory/CameraFactory/CameraFactory.cpp
@@ -16,13 +16,26 @@ std::shared_ptr<RayTracer::Camera> RayTracer::Factory::CameraFactory::createCame
 
     camera->resolution = std::make_tuple(width, height);
     camera->fov = fov;
+
+    double aspectRatio = static_cast<double>(width) / static_cast<double>(height);
+    Math::Point3D screenOrigin(0, 0, -1);
+    Math::Vector3D bottom(aspectRatio, 0, 0);  // Bottom vector prend en compte le ratio d'aspect
+    Math::Vector3D left(0, 1, 0);
+    camera->screen = RayTracer::Rectangle3D(screenOrigin, bottom, left);
+
+    camera->transformMatrix = Math::Matrix4x4();
+
+    camera->rotateZ(rotZ);
+    camera->rotateY(rotY);
+    camera->rotateX(rotX);
+
     camera->setPosition(posX, posY, posZ);
-    camera->setRotation(rotX, rotY, rotZ);
 #ifdef _DEBUG
     std::cout << "Camera created with position (" << posX << ", " << posY << ", " << posZ 
               << "), rotation (" << rotX << ", " << rotY << ", " << rotZ 
               << "), resolution " << width << "x" << height 
-              << ", FOV " << fov << std::endl;
+              << ", FOV " << fov 
+              << ", aspect ratio " << aspectRatio << std::endl;
 #endif
 
     return camera;
