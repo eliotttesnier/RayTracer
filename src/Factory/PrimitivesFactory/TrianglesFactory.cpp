690dcb9ce2c355b2a06b37bfbdac8cfb5eff9364
Nolan Papa <nolan.papa@epitech.eu>
2025-05-07 15:46:39 +0200

    [FIX] improve code formatting in TrianglesFactory.cpp for better readability

--- src/Factory/PrimitivesFactory/TrianglesFactory.cpp
+++ src/Factory/PrimitivesFactory/TrianglesFactory.cpp
@@ -6,24 +6,39 @@
 */
 
 #include "TrianglesFactory.hpp"
+
+#include <map>
+#include <memory>
+#include <string>
+
 #include "Primitives/Triangles/Triangles.hpp"
 
-RayTracer::Factory::TrianglesFactory::TrianglesFactory(const Math::Point3D &p1, const Math::Point3D &p2, const Math::Point3D &p3, const Math::Vector3D &rotation, const Math::Vector3D &scale
-):
+RayTracer::Factory::TrianglesFactory::TrianglesFactory(
+            const Math::Point3D &p1,
+            const Math::Point3D &p2,
+            const Math::Point3D &p3,
+            const Math::Vector3D &rotation,
+            const Math::Vector3D &scale):
     _p1(p1),
     _p2(p2),
     _p3(p3),
     _rotation(rotation),
     _scale(scale)
 {
-
 }
 
-std::shared_ptr<IPrimitive> RayTracer::Factory::TrianglesFactory::create(std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins) const
+std::shared_ptr<IPrimitive> RayTracer::Factory::TrianglesFactory::create(
+            std::map<std::string,
+            std::unique_ptr<Loader::LibLoader>> &plugins) const
 {
     if (!plugins.contains("Triangles"))
         throw std::runtime_error("Triangles plugin not found");
-    auto obj = plugins["Triangles"]->initEntryPointPtr<primitive::Triangles>("create", this->_p1, this->_p2, this->_p3);
+    auto obj = plugins["Triangles"]->initEntryPointPtr<primitive::Triangles>(
+        "create",
+        this->_p1,
+        this->_p2,
+        this->_p3
+    );
     obj->setRotation(this->_rotation);
     obj->setScale(this->_scale);
     return std::shared_ptr<IPrimitive>(obj, [](IPrimitive* ptr) { delete ptr; });
