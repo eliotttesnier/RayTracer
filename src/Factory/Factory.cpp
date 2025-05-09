0ff626534e3af6bf3871065725f3ba1aba9832e9
albanrss <alban.roussee@epitech.eu>
2025-05-09 16:10:39 +0200

    [ADD] Introduce materials attribute for primitives and refactor Factory methods for better organization

--- src/Factory/Factory.cpp
+++ src/Factory/Factory.cpp
@@ -15,26 +15,33 @@
 #include "LigthsFactory/LightFactory.hpp"
 #include "PrimitivesFactory/PrimitiveFactory.hpp"
 
-std::tuple<std::vector<std::shared_ptr<IPrimitive>>,
-           std::vector<std::shared_ptr<ILight>>,
-           std::shared_ptr<RayTracer::Camera>>
-                RayTracer::Factory::Factory::createElement(
-                    const Parser::CameraConfig &camera,
-                    const Parser::PrimitivesConfig &primitives,
-                    const Parser::LightsConfig &light,
-                    std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins)
+RayTracer::Factory::scene_t RayTracer::Factory::Factory::createElement(
+    const Parser::CameraConfig &camera,
+    const Parser::PrimitivesConfig &primitives,
+    const Parser::LightsConfig &light,
+    std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins
+)
 {
-#ifdef _DEBUG
-    std::cout << std::endl << "Factory running..." << std::endl;
-#endif
-    auto primitivesFacto =
-        RayTracer::Factory::PrimitiveFactory::createPrimitives(primitives, plugins);
-    auto cameraFacto = RayTracer::Factory::CameraFactory::createCamera(camera);
-    auto lightFacto = RayTracer::Factory::LightFactory::createLights(light, plugins);
-#ifdef _DEBUG
-    std::cout << std::endl;
-#endif
-    return std::make_tuple(std::move(primitivesFacto),
-           std::move(lightFacto),
-           std::move(cameraFacto));
+    #ifdef _DEBUG
+        std::cout << std::endl << "Factory running..." << std::endl;
+    #endif
+    auto primitivesFacto = RayTracer::Factory::PrimitiveFactory::createPrimitives(
+        primitives,
+        plugins
+    );
+    auto cameraFacto = RayTracer::Factory::CameraFactory::createCamera(
+        camera
+    );
+    auto lightFacto = RayTracer::Factory::LightFactory::createLights(
+        light,
+        plugins
+    );
+    #ifdef _DEBUG
+        std::cout << std::endl;
+    #endif
+    return std::make_tuple(
+        std::move(primitivesFacto),
+        std::move(lightFacto),
+        std::move(cameraFacto)
+    );
 }
