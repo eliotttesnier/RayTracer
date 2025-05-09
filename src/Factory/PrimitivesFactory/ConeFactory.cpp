66ec0335c7a173de734c1473b51118d17ceda1cf
albanrss <alban.roussee@epitech.eu>
2025-05-09 18:12:05 +0200

    [UPT] ConeFactory: add materials parameter to constructor and set material in create method

--- src/Factory/PrimitivesFactory/ConeFactory.cpp
+++ src/Factory/PrimitivesFactory/ConeFactory.cpp
@@ -7,34 +7,49 @@
 #include <string>
 #include <stdexcept>
 #include "ConeFactory.hpp"
+#include "../MaterialFactory/MaterialFactory.hpp"
 #include "Primitives/Cone/Cone.hpp"
 
-RayTracer::Factory::ConeFactory::ConeFactory(const Math::Point3D &position,
-                                             const Math::Vector3D &rotation,
-                                             const Math::Vector3D &scale,
-                                             const Math::Vector3D &shear,
-                                             double radius, double height):
+RayTracer::Factory::ConeFactory::ConeFactory(
+    const Math::Point3D &position,
+    const Math::Vector3D &rotation,
+    const Math::Vector3D &scale,
+    const Math::Vector3D &shear,
+    double radius,
+    double height,
+    const std::vector<std::string> &materials
+):
     _radius(radius),
     _height(height),
     _position(position),
     _rotation(rotation),
     _scale(scale),
-    _shear(shear)
+    _shear(shear),
+    _materials(materials)
 {
 }
 
 std::shared_ptr<IPrimitive> RayTracer::Factory::ConeFactory::create(
-            std::map<std::string,
-            std::unique_ptr<Loader::LibLoader>> &plugins) const
+    std::map<std::string,
+    std::unique_ptr<Loader::LibLoader>> &plugins
+) const
 {
     if (plugins.find("Cone") == plugins.end())
         throw std::runtime_error("Cone plugin not found");
-    auto obj = plugins["Cone"]->initEntryPointPtr<primitive::Cone>("create",
-                                                                   this->_position,
-                                                                   this->_radius,
-                                                                   this->_height);
+    auto obj = plugins["Cone"]->initEntryPointPtr<primitive::Cone>(
+        "create",
+        this->_position,
+        this->_radius,
+        this->_height
+    );
     obj->setRotation(this->_rotation);
     obj->setScale(this->_scale);
     obj->setShear(this->_shear);
+    std::shared_ptr<RayTracer::Materials::IMaterial> material =
+        RayTracer::Factory::MaterialFactory::createMaterial(
+        this->_materials,
+        plugins
+    );
+    obj->setMaterial(material);
     return std::shared_ptr<IPrimitive>(obj, [](IPrimitive* ptr) { delete ptr; });
 }
