46a0efc8ce1211dd31055128a45d62c2bb190d3e
albanrss <alban.roussee@epitech.eu>
2025-05-09 18:10:15 +0200

    [UPT] OBJ: refactor getColor method to accept hit data, ray, lights, and primitives

--- src/Primitives/OBJ/OBJ.cpp
+++ src/Primitives/OBJ/OBJ.cpp
@@ -185,11 +185,20 @@ Math::hitdata_t OBJ::intersect(const Math::Ray &ray)
     return closestHit;
 }
 
-Graphic::color_t Cone::getColor(
-    RayTracer::Materials::infos_t infos
+Graphic::color_t OBJ::getColor(
+    Math::hitdata_t hitData,
+    Math::Ray ray,
+    std::vector<std::shared_ptr<ILight>> lights,
+    std::vector<std::shared_ptr<IPrimitive>> primitives
 )
 {
-    return _material->calculateColor(*this, infos);
+    return _material->calculateColor(
+        *this,
+        hitData,
+        ray,
+        lights,
+        primitives
+    );
 }
 
 }  // namespace RayTracer::primitive
--- src/Primitives/OBJ/OBJ.hpp
+++ src/Primitives/OBJ/OBJ.hpp
@@ -29,7 +29,10 @@ class OBJ : public APrimitive {
         void loadFromFile();
 
         Graphic::color_t getColor(
-            RayTracer::Materials::infos_t infos
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
         ) override;
 
     private:
