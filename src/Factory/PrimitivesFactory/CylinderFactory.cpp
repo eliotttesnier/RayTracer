d5dcf3875f0f9ff7492433d8c7a87636b8e55569
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-09 12:11:02 +0200

    [ADD] CylinderFactory taking shear

--- src/Factory/PrimitivesFactory/CylinderFactory.cpp
+++ src/Factory/PrimitivesFactory/CylinderFactory.cpp
@@ -16,13 +16,15 @@ RayTracer::Factory::CylinderFactory::CylinderFactory(
             const Math::Point3D &position,
             const Math::Vector3D &rotation,
             const Math::Vector3D &scale,
+            const Math::Vector3D &shear,
             double radius,
             double height):
     _radius(radius),
     _height(height),
     _position(position),
     _rotation(rotation),
-    _scale(scale)
+    _scale(scale),
+    _shear(shear)
 {
 }
 
@@ -40,5 +42,6 @@ std::shared_ptr<IPrimitive> RayTracer::Factory::CylinderFactory::create(
     );
     obj->setRotation(this->_rotation);
     obj->setScale(this->_scale);
+    obj->setShear(this->_shear);
     return std::shared_ptr<IPrimitive>(obj, [](IPrimitive* ptr) { delete ptr; });
 }
--- src/Factory/PrimitivesFactory/CylinderFactory.hpp
+++ src/Factory/PrimitivesFactory/CylinderFactory.hpp
@@ -13,7 +13,9 @@ namespace RayTracer::Factory {
 
     class CylinderFactory : public IPrimitiveFactory {
     public:
-        CylinderFactory(const Math::Point3D &position, const Math::Vector3D &rotation, const Math::Vector3D &scale, double radius, double height);
+        CylinderFactory(const Math::Point3D &position, const Math::Vector3D &rotation,
+                       const Math::Vector3D &scale, const Math::Vector3D &shear,
+                       double radius, double height);
 
         std::shared_ptr<IPrimitive> create(std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins) const override;
     private:
@@ -22,6 +24,7 @@ namespace RayTracer::Factory {
         Math::Point3D _position;
         Math::Vector3D _rotation;
         Math::Vector3D _scale;
+        Math::Vector3D _shear;
     };
 
 } // namespace Raytracer::Factory
