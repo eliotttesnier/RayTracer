59ccf19d4b310f2fbc367f309b60c48ed6c49818
albanrss <alban.roussee@epitech.eu>
2025-05-12 11:42:24 +0200

    [ADD] TransparencyMaterial: add CMake configuration and entry point for the TransparencyMaterial library

---
+++ src/Materials/Transparency/Entrypoint.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** rayTracer
+** File description:
+** Entrypoint
+*/
+
+#include <unistd.h>
+
+#include <iostream>
+
+#include "LoaderType.hpp"
+#include "TransparencyMaterial.hpp"
+
+extern "C" {
+    RayTracer::Materials::TransparencyMaterial *create(
+        std::shared_ptr<RayTracer::Materials::IMaterial> wrappee,
+        double transparency
+    ) {
+        return new RayTracer::Materials::TransparencyMaterial(wrappee, transparency);
+    }
+    RayTracer::Loader::ModuleType getType(void) {
+        return RayTracer::Loader::ModuleType::MATERIAL_MODULE;
+    }
+    __attribute__((constructor)) void construct() {
+        #ifdef _DEBUG
+                std::cout << "Loaded dynamic lib: Lib TransparencyMaterial" << std::endl;
+        #endif
+    }
+    __attribute__((destructor)) void deconstruct() {
+        #ifdef _DEBUG
+                std::cout << "Unloaded dynamic lib: Lib TransparencyMaterial" << std::endl;
+        #endif
+    }
+}
