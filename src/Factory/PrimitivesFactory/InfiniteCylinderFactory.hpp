e4691e9820636a7f0d7b625b9ec9077b8dd5f91f
albanrss <alban.roussee@epitech.eu>
2025-05-12 17:44:25 +0200

    [UPT] InfiniteCylinderFactory: change materials type from vector<string> to Math::Vector3D

--- src/Factory/PrimitivesFactory/InfiniteCylinderFactory.hpp
+++ src/Factory/PrimitivesFactory/InfiniteCylinderFactory.hpp
@@ -24,7 +24,7 @@ namespace RayTracer::Factory {
             const Math::Vector3D &scale,
             const Math::Vector3D &shear,
             double radius,
-            const std::vector<std::string> &materials
+            const Math::Vector3D &materials
         );
 
         std::shared_ptr<IPrimitive> create(std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins) const override;
@@ -34,7 +34,7 @@ namespace RayTracer::Factory {
         Math::Vector3D _rotation;
         Math::Vector3D _scale;
         Math::Vector3D _shear;
-        std::vector<std::string> _materials;
+        Math::Vector3D _materials;
     };
 
 }  // namespace Raytracer::Factory
