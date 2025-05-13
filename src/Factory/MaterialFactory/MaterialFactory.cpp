8f6495ab89dc354e7939fd2e6b76a124649329e3
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-13 14:26:52 +0200

    [MOD] Material Factory any_casting to real type

--- src/Factory/MaterialFactory/MaterialFactory.cpp
+++ src/Factory/MaterialFactory/MaterialFactory.cpp
@@ -11,11 +11,12 @@
 #include <string>
 #include <memory>
 #include <iostream>
+#include <any>
 
 namespace RayTracer::Factory {
 
 std::shared_ptr<RayTracer::Materials::IMaterial> MaterialFactory::createMaterial(
-    const std::map<std::string, double> &materialProps,
+    const std::map<std::string, std::any> &materialProps,
     std::map<std::string, std::unique_ptr<Loader::LibLoader>>& plugins
 )
 {
@@ -28,7 +29,7 @@ std::shared_ptr<RayTracer::Materials::IMaterial> MaterialFactory::createMaterial
     ));
 
     auto chessIt = materialProps.find("chess");
-    if (chessIt != materialProps.end() && chessIt->second > 0.0) {
+    if (chessIt != materialProps.end() && std::any_cast<bool>(chessIt->second)) {
         materialStack.emplace_back(std::shared_ptr<RayTracer::Materials::IMaterial>(
             plugins["ChessPatternMaterial"]->
                 initEntryPointPtr<RayTracer::Materials::ChessPatternMaterial>(
@@ -39,24 +40,26 @@ std::shared_ptr<RayTracer::Materials::IMaterial> MaterialFactory::createMaterial
     }
 
     auto transparencyIt = materialProps.find("transparency");
-    if (transparencyIt != materialProps.end() && transparencyIt->second > 0.0) {
+    if (transparencyIt != materialProps.end() && std::any_cast<double>(transparencyIt->second) > 0.0) {
+        double transparency = std::any_cast<double>(transparencyIt->second);
         materialStack.emplace_back(std::shared_ptr<RayTracer::Materials::IMaterial>(
             plugins["TransparencyMaterial"]->
                 initEntryPointPtr<RayTracer::Materials::TransparencyMaterial>(
                     "create",
                     materialStack.at(materialStack.size() - 1),
-                    transparencyIt->second
+                    transparency
                 )
         ));
     }
 
     auto reflectionIt = materialProps.find("reflection");
-    if (reflectionIt != materialProps.end() && reflectionIt->second > 0.0) {
+    if (reflectionIt != materialProps.end() && std::any_cast<double>(reflectionIt->second) > 0.0) {
+        // double reflection = std::any_cast<double>(reflectionIt->second);
         // materialStack.emplace_back(
         //     std::shared_ptr<RayTracer::Materials::IMaterial>(
         //         plugins["ReflectionMaterial"]->initEntryPointPtr<RayTracer::Materials::ReflectionMaterial>(
         //             "create",
-        //             reflectionIt->second
+        //             reflection
         //         )
         //     )
         // );
@@ -66,12 +69,13 @@ std::shared_ptr<RayTracer::Materials::IMaterial> MaterialFactory::createMaterial
     }
 
     auto refractionIt = materialProps.find("refraction");
-    if (refractionIt != materialProps.end() && refractionIt->second > 0.0) {
+    if (refractionIt != materialProps.end() && std::any_cast<double>(refractionIt->second) > 0.0) {
+        // double refraction = std::any_cast<double>(refractionIt->second);
         // materialStack.emplace_back(
         //     std::shared_ptr<RayTracer::Materials::IMaterial>(
         //         plugins["RefractionMaterial"]->initEntryPointPtr<RayTracer::Materials::RefractionMaterial>(
         //             "create",
-        //             refractionIt->second
+        //             refraction
         //         )
         //     )
         // );
