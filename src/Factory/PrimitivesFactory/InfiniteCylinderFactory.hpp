c7984d93027f4e4d9b8c327de045279bb0c91520
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-13 14:27:51 +0200

    [MOD] Material map of string/any in InfiniteCylinder

--- src/Factory/PrimitivesFactory/InfiniteCylinderFactory.hpp
+++ src/Factory/PrimitivesFactory/InfiniteCylinderFactory.hpp
@@ -10,6 +10,7 @@
 
 #include <memory>
 #include <map>
+#include <any>
 #include "IPrimitiveFactory.hpp"
 #include "../../Primitives/InfiniteCylinder/InfiniteCylinder.hpp"
 #include "LibLoader/LibLoader.hpp"
@@ -24,7 +25,7 @@ namespace RayTracer::Factory {
             const Math::Vector3D &scale,
             const Math::Vector3D &shear,
             double radius,
-            const std::map<std::string, double> &materials
+            const std::map<std::string, std::any> &materials
         );
 
         std::shared_ptr<IPrimitive> create(std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins) const override;
@@ -34,7 +35,7 @@ namespace RayTracer::Factory {
         Math::Vector3D _rotation;
         Math::Vector3D _scale;
         Math::Vector3D _shear;
-        std::map<std::string, double> _materials;
+        std::map<std::string, std::any> _materials;
     };
 
 }  // namespace Raytracer::Factory
