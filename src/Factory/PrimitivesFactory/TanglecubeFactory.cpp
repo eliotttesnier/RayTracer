59cba578e02ed33e4dfb4c2d30e1e1dcccb412f5
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-13 11:41:13 +0200

    [MOD] Map of material in Tanglecube

--- src/Factory/PrimitivesFactory/TanglecubeFactory.cpp
+++ src/Factory/PrimitivesFactory/TanglecubeFactory.cpp
@@ -19,7 +19,7 @@ RayTracer::Factory::TanglecubeFactory::TanglecubeFactory(
     const Math::Vector3D &scale,
     const Math::Vector3D &shear,
     double size,
-    const Math::Vector3D &materials
+    const std::map<std::string, double> &materials
 ):
     _size(size),
     _position(position),
