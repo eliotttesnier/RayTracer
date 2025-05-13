7f0119b4fddd6870dd4e5aebf52e6fe057936444
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-13 15:46:34 +0200

    [ADD] FileTexture Material Implementation

---
+++ src/Materials/FileTexture/FileTextureMaterial.hpp
@@ -0,0 +1,129 @@
+/*
+** EPITECH PROJECT, 2025
+** rayTracer
+** File description:
+** FileTextureMaterial
+*/
+
+#ifndef FILETEXTUREMATERIAL_HPP_
+#define FILETEXTUREMATERIAL_HPP_
+
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+#include "../../Primitives/Cone/Cone.hpp"
+#include "../../Primitives/Cylinder/Cylinder.hpp"
+#include "../../Primitives/Plane/Plane.hpp"
+#include "../../Primitives/Sphere/Sphere.hpp"
+#include "../../Primitives/Torus/Torus.hpp"
+#include "../../Primitives/Tanglecube/Tanglecube.hpp"
+#include "../../Primitives/Triangles/Triangles.hpp"
+#include "../../Primitives/OBJ/OBJ.hpp"
+#include "../../Primitives/FractaleCube/FractaleCube.hpp"
+#include "../../Primitives/InfiniteCone/InfiniteCone.hpp"
+#include "../../Primitives/InfiniteCylinder/InfiniteCylinder.hpp"
+
+#include "../AMaterial.hpp"
+
+namespace RayTracer::Materials {
+
+class FileTextureMaterial : public AMaterial {
+    public:
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Cone &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Cylinder &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Plane &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Sphere &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Torus &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Tanglecube &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::Triangles &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::OBJ &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::FractaleCube &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::InfiniteCone &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+        Graphic::color_t calculateColor(
+            const RayTracer::primitive::InfiniteCylinder &obj,
+            Math::hitdata_t hitData,
+            Math::Ray ray,
+            std::vector<std::shared_ptr<ILight>> lights,
+            std::vector<std::shared_ptr<IPrimitive>> primitives
+        ) override;
+
+        FileTextureMaterial();
+        FileTextureMaterial(std::shared_ptr<IMaterial> wrappee, std::string filePath);
+        FileTextureMaterial(std::shared_ptr<IMaterial> wrappee, std::string filePath, float textureScale);
+        virtual ~FileTextureMaterial();
+
+        void setTextureScale(float scale) { _textureScale = scale; }
+
+    private:
+        std::shared_ptr<IMaterial> _wrappee;
+        std::string _filePath;
+        sf::Image _textureImage;
+        sf::Vector2u _textureSize;
+        bool _textureLoaded = false;
+        float _textureScale = 1.0f;
+        Graphic::color_t getTextureColorAtUV(float u, float v) const;
+};
+
+}
+
+#endif /* !FILETEXTUREMATERIAL_HPP_ */
