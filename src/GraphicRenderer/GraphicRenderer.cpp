39c48dd268143a197ad6064afe93a074597e67de
eliotttesnier <eliott.tesnier@epitech.eu>
2025-05-15 16:33:06 +0200

    [FIX] No more getenv / setenv but enum in Graphic Renderer

--- src/GraphicRenderer/GraphicRenderer.cpp
+++ src/GraphicRenderer/GraphicRenderer.cpp
@@ -197,22 +197,23 @@ void GraphicRenderer::exportToPNG(const std::string& outputFilename) const
     }
 }
 
-bool GraphicRenderer::handleCamMovement(sf::Event event)
+runResult_e GraphicRenderer::handleCamMovement(sf::Event event)
 {
     if (event.type != sf::Event::KeyPressed)
-        return false;
+        return NOTHING;
 
+    runResult_e result = NOTHING;
     switch (event.key.code) {
-        case sf::Keyboard::Z: setenv("RAYTRACER_CAMERA_MOVE", "forward", 1); break;
-        case sf::Keyboard::S: setenv("RAYTRACER_CAMERA_MOVE", "backward", 1); break;
-        case sf::Keyboard::D: setenv("RAYTRACER_CAMERA_MOVE", "right", 1); break;
-        case sf::Keyboard::Q: setenv("RAYTRACER_CAMERA_MOVE", "left", 1); break;
-        case sf::Keyboard::A: setenv("RAYTRACER_CAMERA_MOVE", "up", 1); break;
-        case sf::Keyboard::E: setenv("RAYTRACER_CAMERA_MOVE", "down", 1); break;
-        default: return false;
+        case sf::Keyboard::Z: result = MOVE_FORWARD; break;
+        case sf::Keyboard::S: result = MOVE_BACKWARD; break;
+        case sf::Keyboard::D: result = MOVE_RIGHT; break;
+        case sf::Keyboard::Q: result = MOVE_LEFT; break;
+        case sf::Keyboard::A: result = MOVE_UP; break;
+        case sf::Keyboard::E: result = MOVE_DOWN; break;
+        default: return NOTHING;
     }
     _window.close();
-    return true;
+    return result;
 }
 
 runResult_e GraphicRenderer::run(std::atomic<bool>& renderingComplete)
@@ -253,8 +254,10 @@ runResult_e GraphicRenderer::run(std::atomic<bool>& renderingComplete)
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
                 _window.close();
 
-            if (handleCamMovement(event)) {
-                result = RELOAD_CONFIG;
+            runResult_e camMovementResult = handleCamMovement(event);
+            if (camMovementResult != NOTHING) {
+                result = camMovementResult;
+                _window.close();
                 return result;
             }
         }
