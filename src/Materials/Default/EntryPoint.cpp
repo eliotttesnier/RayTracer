317f37a95b01dc36adb9d05d6cb7c1f0f240d7ea
albanrss <alban.roussee@epitech.eu>
2025-05-15 19:49:11 +0200

    [UPT] EntryPoint: Update create function to accept shading parameter for DefaultMaterial

--- src/Materials/Default/EntryPoint.cpp
+++ src/Materials/Default/EntryPoint.cpp
@@ -13,22 +13,24 @@
 #include "DefaultMaterial.hpp"
 
 extern "C" {
-    RayTracer::Materials::DefaultMaterial *create() {
-        return new RayTracer::Materials::DefaultMaterial();
+    RayTracer::Materials::DefaultMaterial *create(const RayTracer::Materials::shading_t &shading) {
+        return new RayTracer::Materials::DefaultMaterial(
+            shading
+        );
     }
     RayTracer::Loader::ModuleType getType(void) {
         return RayTracer::Loader::ModuleType::MATERIAL_MODULE;
     }
     __attribute__((constructor)) void construct()
     {
-#ifdef _DEBUG
-        std::cout << "Loaded dynamic lib: Lib DefaultMaterial" << std::endl;
-#endif
+        #ifdef _DEBUG
+                std::cout << "Loaded dynamic lib: Lib DefaultMaterial" << std::endl;
+        #endif
     }
     __attribute__((destructor)) void deconstruct()
     {
-#ifdef _DEBUG
-        std::cout << "Unloaded dynamic lib: Lib DefaultMaterial" << std::endl;
-#endif
+        #ifdef _DEBUG
+                std::cout << "Unloaded dynamic lib: Lib DefaultMaterial" << std::endl;
+        #endif
     }
 }
