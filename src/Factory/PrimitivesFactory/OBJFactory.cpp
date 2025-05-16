d5c8e20f735ad791ef947efd8632e3a00c30f253
albanrss <alban.roussee@epitech.eu>
2025-05-16 16:08:50 +0200

    [UPT] OBJFactory: change plugin parameter type from unique_ptr to shared_ptr for improved memory management

--- src/Factory/PrimitivesFactory/OBJFactory.cpp
+++ src/Factory/PrimitivesFactory/OBJFactory.cpp
@@ -33,7 +33,7 @@ RayTracer::Factory::OBJFactory::OBJFactory(
 }
 
 std::shared_ptr<IPrimitive> RayTracer::Factory::OBJFactory::create(
-    std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins
+    std::map<std::string, std::shared_ptr<Loader::LibLoader>> plugins
 ) const
 {
     if (plugins.find("OBJ") == plugins.end())
