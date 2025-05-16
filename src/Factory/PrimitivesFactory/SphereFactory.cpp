7b290c8ba8aafbcc866c8096fd274576e60b4b2d
albanrss <alban.roussee@epitech.eu>
2025-05-16 16:09:19 +0200

    [UPT] SphereFactory: change plugin parameter type from unique_ptr to shared_ptr for improved memory management

--- src/Factory/PrimitivesFactory/SphereFactory.cpp
+++ src/Factory/PrimitivesFactory/SphereFactory.cpp
@@ -33,7 +33,7 @@ RayTracer::Factory::SphereFactory::SphereFactory(
 }
 
 std::shared_ptr<IPrimitive> RayTracer::Factory::SphereFactory::create(
-    std::map<std::string, std::unique_ptr<Loader::LibLoader>> &plugins
+    std::map<std::string, std::shared_ptr<Loader::LibLoader>> plugins
 ) const
 {
     if (plugins.find("Sphere") == plugins.end())
